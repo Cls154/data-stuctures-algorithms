60ecb2e7a6baf6622a919a162cbb4be57fda176b
Cieran Sung <clsung154@gmail.com>
2022-10-09 13:10:10 +0100

    first-commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const unsortedArray = [9, 5, 2, 7, 4, 1, 3, 6, 8];
+
+function selection(array) {
+  for (let i=0; i<array.length; i++) {
+    let minIndex = i;
+    for (let j=i+1; j<array.length; j++) {
+      if (array[minIndex] > array[j]) {
+        minIndex = j;
+      }
+    }
+    let temp = array[minIndex];
+    array[minIndex] = array[i];
+    array[i] = temp;
+  }
+
+  console.log(array);
+}
+
+selection(unsortedArray);
